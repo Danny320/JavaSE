97498110151b87d94f88471aa002c03aab750e9e
Danny320 <2261536364@qq.com>
2020-04-07 21:02:21 +0800

    第一个java程序

---
+++ 20200407/HelloWord.java
@@ -0,0 +1,40 @@
+
+
+public class HelloWord{
+
+	public static void main(String[] args) {
+		 int num1 = 10;
+		 System.out.println(num1);
+		 System.out.println(Integer.MAX_VALUE);
+		 System.out.println(Integer.MIN_VALUE);
+		 long num2 = 10l;
+         System.out.println(num2);
+         System.out.println(Long.MAX_VALUE);
+         System.out.println(Long.MIN_VALUE);
+         double num3 =1.0;
+         System.out.println(num3);
+         String a="hello";
+         String b="world";
+         String c=a+b;
+         System.out.println(c);
+         String greeting ="hello";
+         String s=greeting.substring(0,3);
+         System.out.println(s);
+         String expletive ="Expletive";
+         String PG13 ="deleted";
+         String message =expletive+PG13;
+         System.out.println(message);
+         int age = 13;
+         String rating ="PG"+age;
+         System.out.println(rating);
+         System.out.println("as"+10);
+         greeting =greeting.substring(0,3)+"p !";
+         System.out.println(greeting);
+         
+         char* temp = malloc(6);
+         strncpy(temp,greeting,3);
+         strncpy(tmp+3,"p!",3);
+         greeting = temp;
+
+	}
+}
