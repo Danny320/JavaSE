bda10f38cf14bd6984b04dce898ffe4429fccc8b
Danny320 <2261536364@qq.com>
2020-04-12 23:26:12 +0800

    1

--- 20200411/TestDemo3.java
+++ 20200411/TestDemo3.java
@@ -2,6 +2,89 @@ import java.util.Scanner;
 public class TestDemo3{
 
 public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int num4 = sc.nextInt();
+	int ret5 = fib(num4);
+	System.out.println(ret5);
+}
+public static int fib(int num4){
+	if(num4 <= 2){
+		return 1;
+	}
+	int last1 = 1;
+	int last2 = 1;
+	int cur = 0;
+	for(int i = 3;i < = num4;i++){
+		cur = last1 + last2;
+		last1 = last2;
+		last2 = cur;
+	}
+	return cur;
+}
+
+
+
+public static void main10(String[] args) {
+	int num3 = 1729;
+	int ret4 = sum1(num3);
+	System.out.println(ret4);
+}
+public static int sum1(int num3){
+	if(num3 < 10){
+		return num3; 
+	}
+	return num3 % 10 + sum1(num3 / 10);
+}
+
+
+
+public static void main9(String[] args) {
+	int num2 = 10;
+	int ret3 = sum(num2);
+	System.out.println(ret3);
+}
+public static int sum(int num2){
+	if(num2 == 1){
+		return 1; 
+	}
+	return num2+sum(num2-1);
+}
+
+
+public static void main8(String[] args) {
+	int num1 = 1234;
+	 print(num1);
+}
+public static void print(int num1) {
+	if(num1 > 9) {
+		print(num1 / 10);
+	}
+	System.out.println(num1 % 10);
+}
+
+
+
+public static void main7(String[] args) {
+	int a = 10;
+	int b = 20;
+	int ret = add1(a,b);
+	System.out.println(ret);
+
+	double a2 = 10.5;
+    double b2 = 20.5;
+    double ret2 = add1(a2,b2);
+	System.out.println(ret2); 
+}
+public static int add1(int x,int y){
+	return x + y;
+}
+public static double add1(double x,double y){
+	return x + y;
+}
+
+
+
+public static void main6(String[] args) {
 	int n = 5;
 	int ret = factor1(n);
 	System.out.println("ret = "+ret);
