a328a910a2686fe4acbb9aac6464e96a2019dd5d
Danny320 <2261536364@qq.com>
2020-04-18 22:24:26 +0800

    1

--- 20200417/src/TestDemo.java
+++ 20200417/src/TestDemo.java
@@ -6,7 +6,33 @@
  * @Date: 2020-04-17
  * @time: 16:14
  **/
-public class TestDemo {
 
-    
+
+class Person {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+public class TestDemo {
+    public static void main(String[] args) {
+        Person per = new Person();//实例化一个对象
+        per.setAge(18);
+        per.setName("罗丹");
+       System.out.println( per.toString());
+    }
 }
