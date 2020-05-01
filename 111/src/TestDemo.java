61f9f2663458832528fc8e039d4d386f08f0097d
Danny320 <2261536364@qq.com>
2020-05-01 22:51:26 +0800

    1

---
+++ 111/src/TestDemo.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+/**
+ * Created with Intellij IDEA
+ *
+ * @program: 111
+ * @Description:
+ * @USer:LD
+ * @Date: 2020-05-01 17:26
+ * @time: 17:26
+ **/
+public class TestDemo {
+    public static void main(String[] args) {
+        Date date = new Date();
+        String s = date.toString();
+        System.out.println(s);
+
+
+    }
+}
+
