5e1a3c1bd0091ff16d0a8d8c1cd4d3ab2088efa7
Danny320 <2261536364@qq.com>
2020-05-01 22:57:58 +0800

    顺序表

--- 20200424/src/TestDemo.java
+++ 20200424/src/TestDemo.java
@@ -8,8 +8,20 @@
  * @time: 19:52
  **/
 public class TestDemo {
-    public boolean hasCycle() {
-
+    public static void main(String[] args) {
+        MyArrayList myArrayList1 = new MyArrayList();
+        //MyArrayList myArrayList2 = new MyArrayList();
+        myArrayList1.add(0,1);
+        myArrayList1.add(1,2);
+        myArrayList1.add(2,3);
+        myArrayList1.add(3,4);
+        myArrayList1.display();
+        boolean ret = myArrayList1.contains(1);
+        System.out.println( ret);
+        System.out.println(myArrayList1.search(3));
+        System.out.println(myArrayList1.getPos(3));
+        myArrayList1.remove(3);
+        myArrayList1.display();
     }
 
 }
