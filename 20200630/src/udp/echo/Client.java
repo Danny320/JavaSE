e7b40144fa4a29b9f5e3778d12d0421c7206f92f
Danny320 <2261536364@qq.com>
2020-06-30 23:25:56 +0800

    1

---
+++ 20200630/src/udp/echo/Client.java
@@ -0,0 +1,16 @@
+package udp.echo;
+
+/**
+ * Created with Intellij IDEA
+ *
+ * @program: 20200630
+ * @Description:
+ * @USer:LD
+ * @Date: 2020-06-30 22:18
+ * @time: 22:18
+ **/
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 20200630/src/udp/echo/Server.java
@@ -0,0 +1,18 @@
+package udp.echo;
+
+/**
+ * Created with Intellij IDEA
+ *
+ * @program: 20200630
+ * @Description:
+ * @USer:LD
+ * @Date: 2020-06-30 22:17
+ * @time: 22:17
+ **/
+public class Server {
+    //作为服务端，需要公开自己的地址
+    //ip
+    public static void main(String[] args) {
+
+    }
+}
