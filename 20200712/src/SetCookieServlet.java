86e00c0efbdc8c27833cca3e499c56572ba83343
Danny320 <2261536364@qq.com>
2020-07-13 23:30:16 +0800

    1

--- 20200712/src/SetCookieServlet.java
+++ 20200712/src/SetCookieServlet.java
@@ -1,5 +1,4 @@
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,12 +6,13 @@ import java.io.IOException;
 
 /**
  * @author LD
- * @date 2020/7/12 16:22
+ * @date 2020/7/13 10:05
  */
-public class StatusServlet extends HttpServlet {
+public class SetCookieServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setStatus(301);
-        resp.setHeader("Location","https://www.baidu.com/");
+        resp.setHeader("Set-Cookie","tody=2020-07-13");
+        resp.setContentType("text/plain; charset=utf-8");
+        resp.getWriter().println("种下cookie：today=2020-07-13");
     }
 }
